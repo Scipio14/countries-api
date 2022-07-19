96554da807c1dea409b4803e9295a8c45f280084
Federico Macías <fma.abogado@gmail.com>
2022-07-18 19:08:21 -0500

    Removing the ejs frontend

--- app.js
+++ app.js
@@ -2,27 +2,17 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const countriesRouter = require("./routes/countries.js");
-const { getCountriesPage } = require("./controllers/countriesController");
 const cors = require("cors");
-const ejs = require("ejs");
+
 
 app.set("port", process.env.PORT || 5000);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
 
 app.use("/api/countries", countriesRouter);
 
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
-});
-
-app.get("/countries", getCountriesPage);
-
 app.get("*", (req, res) => {
-  res.render("404", { title: "404" });
+  res.status(404).json({msg:"Page not Found"});
 });
 
 module.exports = app;
--- controllers/countriesController.js
+++ controllers/countriesController.js
@@ -8,6 +8,7 @@ const getCountries = (req, res) => {
   const results = {};
   results.items = countries.length;
   if (endIndex < countries.length) {
+    results.page = page
     results.next = {
       page: page + 1,
       limit: limit,
@@ -33,31 +34,5 @@ const getCountry = (req, res) => {
   res.json(results);
 };
 
-const getCountriesPage = (req, res) => {
-  const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 5;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-  const results = {};
-  results.items = countries.length;
-  if (endIndex < countries.length) {
-    results.next = {
-      page: page + 1,
-      limit: limit,
-    };
-  }
-  if (startIndex > 0) {
-    results.previous = {
-      page: page - 1,
-      limit: limit,
-    };
-  }
 
-  results.totalPages = Math.ceil(countries.length / limit);
-  allCountries = results.results = countries.slice(startIndex, endIndex);
-  // console.log(results.results[0].name_common);
-  // console.log(allCountries);
-  res.render("countries", { title: "Countries", allCountries, results });
-};
-
-module.exports = { getCountries, getCountry, getCountriesPage };
+module.exports = { getCountries, getCountry };
